7ae31d0fbe333e3fa040684085482f6964e407a5
hn.tien <hntien248@yahoo.com>
2025-02-17 22:14:58 +0700

    last update

--- src/components/ui/AccountNav.jsx
+++ src/components/ui/AccountNav.jsx
@@ -7,7 +7,8 @@ const AccountNav = () => {
   if (subpage === undefined) {
     subpage = 'profile';
   }
-
+  
+          
   const linkClases = (type = null) => {
     let classes =
       'flex justify-center mx-10 md:mx-0 gap-1 py-2 px-6 rounded-full';
@@ -54,7 +55,24 @@ const AccountNav = () => {
         </svg>
         My bookings
       </Link>
-   
+
+      <Link className={linkClases('bookings')} to={`/admin`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+          />
+        </svg>
+        Admin
+      </Link>
     </nav>
   );
 };
